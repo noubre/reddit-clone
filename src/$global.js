0ba7b3a4a336245b1942cf74fb53b901cf85145e
nate <noubre@gmail.com>
2023-08-17 15:27:19 -0700

    Updates

--- src/$global.js
+++
@@ -1,9 +0,0 @@
-// Global Scope
-
-var $global = {
-    newSubmit: false,
-    currentPage: 'home', // act as router to snapshot the page currently in (available: 'home', 'newPost')
-    voteChecker: true
-};
-
-export default $global;
--- src/components/Grippy.js
+++
@@ -1,7 +0,0 @@
-// Grippy Handle (Sidebar on the left)
-
-const Grippy = `
-<aside class="grippy"></aside>
-`;
-
-export default Grippy;
